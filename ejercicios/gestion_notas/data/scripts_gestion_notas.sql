c54d8c59b2e386893b8569dd69765e27eed9350c
baileytorch <baileytorch@hotmail.com>
2025-06-24 15:38:32 -0400

    actualizando DML

--- ejercicios/gestion_notas/data/scripts_gestion_notas.sql
+++ ejercicios/gestion_notas/data/scripts_gestion_notas.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS asignaturas(
     CONSTRAINT FK_docentes FOREIGN KEY (id_docente) REFERENCES docentes(id_docente)
 );
 
-INSERT INTO docentes (nombre_asignatura,rut_docente,digito_verificador) VALUES 
+INSERT INTO docentes (nombre_docente,rut_docente,digito_verificador) VALUES 
 ('Aquiles Baeza',12345678,'5'),
 ('Wendy Sulca',13630677,'4'),
 ('Erick Bailey',12824290,'2');
